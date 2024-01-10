bb7a0dd10a8b38db404b9d8b657e9229962de942
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2024-01-10 19:16:00 +0900

    [Gold V] Title: LCS, Time: 164 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        char[] a = new char[s1.length() + 1];
+        char[] b = new char[s2.length() + 1];
+
+        for (int i = 1; i <= s1.length(); i++){
+            a[i] = s1.charAt(i - 1);
+        }
+        for (int i = 1; i <= s2.length(); i++){
+            b[i] = s2.charAt(i - 1);
+        }
+
+        int dp[][] = new int[s1.length()+1][s2.length()+1];
+
+        for (int i = 1; i <= s1.length(); i++){
+            for (int j = 1; j <= s2.length(); j++){
+                if (a[i] == b[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
+
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        System.out.println(dp[s1.length()][s2.length()]);
+        
+    }
+}
