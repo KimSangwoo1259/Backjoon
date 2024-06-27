730613a0a4e33e21db6e7dafde81e46a66c064c2
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2024-06-27 21:41:32 +0900

    [level 2] Title: 짝지어 제거하기, Time: 91.06 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+
+class Solution
+{
+    public int solution(String s)
+    {
+        Stack<Character> st =new Stack<>();
+
+        for (int i = 0; i < s.length(); i++){
+            if (st.isEmpty()){
+                st.push(s.charAt(i));
+            }
+            else{
+                if (st.peek() == s.charAt(i)){
+                    st.pop();
+                }
+                else{
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+        if (st.isEmpty())
+            return 1;
+        return 0;
+    }
+}
