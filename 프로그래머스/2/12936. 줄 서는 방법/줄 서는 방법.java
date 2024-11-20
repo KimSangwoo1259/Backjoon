f7b9961ecfcb738766acf0e4ff6f468c65410d83
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2024-11-20 15:14:31 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.04 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12936. 줄 서는 방법/줄 서는 방법.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        long[] factorial = new long[n+1];
+        boolean[] used = new boolean[n+1];
+        factorial[0] = 1;
+        for(int i = 1; i <= n; i++){
+            factorial[i] = factorial[i - 1] * i;
+        }
+        int index = 0;
+        while(index < n){
+            int value = 0;
+            long temp = factorial[n - (index+1)];
+            for(int i = 1; i <= n - index; i++){
+                if (k <= i * temp){
+                    k -= (i-1) * temp;
+                    for(int j = 1; j <= n; j++){
+                        if (used[j])
+                            continue;
+                        value++;
+                        if (value == i){
+                            answer[index] = j;
+                            used[j] = true;
+                        }
+                    }
+                    break;
+                }
+            }
+            index++;
+        }
+        return answer;
+    }
+}
+// 1 2
