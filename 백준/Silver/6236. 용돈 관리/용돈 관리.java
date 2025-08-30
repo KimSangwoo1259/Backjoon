64e62b6a9c3f290de6559bc1e774bca612e6decd
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2025-08-30 19:06:25 +0900

    [Silver I] Title: 용돈 관리, Time: 236 ms, Memory: 22596 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,51 +1,47 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.valueOf(st.nextToken());
-        int m = Integer.valueOf(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken()); //인출할 횟수
 
         int[] arr = new int[n];
-        int count = 1;
-
-        int max = 0;
+        int answer = 0;
+        
         for (int i = 0; i < n; i++){
-            arr[i] = Integer.valueOf(br.readLine());
-            max =  Math.max(arr[i], max);
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        int left = max;
-        int right = 10000*1000000;
-        int mid = (left + right) / 2;
-        int ans = -1;
+        int left = Arrays.stream(arr).max().getAsInt();
+        int right = 2000000000;
 
-        while (left <= right){
-            int amount = mid;
-            count = 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+
+            int cur = 0;
+            int count = 0;
             for (int i = 0; i < n; i++){
-                if (amount < arr[i]){
-                    amount = mid;
+                if (cur < arr[i]){
+                    cur = mid;
                     count++;
                 }
-                    amount -= arr[i];
-
+                cur -= arr[i];
             }
             if (count <= m){
-                right = mid -1;
-                ans = mid;
+                right = mid - 1;
+                answer = mid;
             }
-            else
+            else {
                 left = mid + 1;
-            mid = (left + right) / 2;
+            }
 
         }
-
-        System.out.println(ans);
-
-
+        System.out.println(answer);
     }
 }
