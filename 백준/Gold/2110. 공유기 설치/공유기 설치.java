fd32af79a0e493123f8b4d0dc8e303323b22f9a8
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2025-08-30 19:58:37 +0900

    [Gold IV] Title: 공유기 설치, Time: 292 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -1,54 +1,47 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.valueOf(st.nextToken());
-        int c = Integer.valueOf(st.nextToken());
-
+        int n = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken()); //설치할 공유기 수
 
         Integer[] arr = new Integer[n];
-
+        int answer = 0;
+        
         for (int i = 0; i < n; i++){
-            arr[i] = Integer.valueOf(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
         int left = 1;
-        int right = arr[n-1] - arr[0];
-        int count = 2; // 공유기 수 카운트 처음과 끝은 디폴트로 넣음
-        int cur = 0; // 최근에 설치한 공유기 위치
-        int mid = (left + right) / 2;
-        int ans = -1;
-        while (left <= right){
+        int right = arr[n - 1] - arr[0];
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            int count = 0;
+            count++;
+            int curPos = 0;
             for (int i = 1; i < n; i++){
-                if (arr[i] - arr[cur] >= mid){
-                    if (count == c - 1){
-                        if (arr[n-1] - arr[i] >= mid){
-                            count++;
-                            cur = i;
-                        }
-                    }
-                    else {
-                        count++;
-                        cur = i;
-                    }
+                int distance = arr[i] - arr[curPos];
+                if (distance >= mid){
+                    curPos = i;
+                    count++;
                 }
             }
             if (count >= c){
-                ans = Math.max(ans,mid);
+                answer = mid;
                 left = mid + 1;
             }
-            else
+            else {
                 right = mid - 1;
-            count = 2;
-            cur = 0;
-            mid = (left + right) / 2;
+            }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
