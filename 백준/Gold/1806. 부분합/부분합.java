af2e452b865ee4267fcd61e3075fabc06039b063
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2025-09-08 21:40:51 +0900

    [Gold IV] Title: 부분합, Time: 256 ms, Memory: 24364 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,56 +1,42 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
 
-        int n = Integer.valueOf(st.nextToken());
-        int s = Integer.valueOf(st.nextToken());
-
-        int[] arr = new int[n+1];
-        int[] sumArr = new int[n+1];
-        int ans = 0;
+        int ans = 100000001;
         st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i <= n; i++){
-            arr[i] = Integer.valueOf(st.nextToken());
+        int[] arr = new int[n];
+        long curSum = 0;
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= n; i++){
-            sumArr[i] = sumArr[i - 1] + arr[i];
-        }
-        if (sumArr[n]>= s){
-            ans = n;
-        }
-        else {
-            System.out.println(ans);
-            return;
-        }
-        int left = 1;
-        int right = 1;
-        int temp;
-        while (right <= n){
-            temp = sumArr[right] - sumArr[left-1];
-            if (temp >= s){
+        int left = 0; int right = 0;
+        curSum = arr[right];
+
+        while (left < n && right < n && (left <= right)){
+            if (curSum >= s){
                 ans = Math.min(ans, right - left + 1);
-                if (left == right){
-                    left++;
-                    right++;
-                }
-                else {
-                    left++;
-                }
-
+                curSum -= arr[left];
+                left++;
             }
             else {
                 right++;
+                if (right < n)
+                    curSum += arr[right];
             }
         }
+          if (ans == 100000001)
+            ans = 0;
         System.out.println(ans);
-
-
     }
 }
