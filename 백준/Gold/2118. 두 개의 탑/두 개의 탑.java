59000e691f00e71ca8fd7524d289b4fd10c4bf6f
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2025-09-11 23:11:11 +0900

    [Gold V] Title: 두 개의 탑, Time: 180 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -1,41 +1,55 @@
 import java.io.*;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int[] arr = new int[n];
+        int[] sum = new int[n];
 
-        int n = Integer.valueOf(br.readLine());
-        int[] arr = new int[n + 1];
-        int ans = 0;
-        int[] sum = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = Integer.valueOf(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        sum[0] = arr[0];
+
+        for (int i = 1; i < n; i++){
+            sum[i] = arr[i] + sum[i - 1];
         }
 
-        for (int i = 1; i <= n; i++) {
-            sum[i] = sum[i - 1] + arr[i];
-        }
-        int totalDistance = sum[n];
-        int temp = 0;
-        int reverse = 0;
         int left = 0;
-        int right = 1;
-
-        while (right <= n) {
-            temp = sum[right] - sum[left];
-            reverse = totalDistance - temp;
-            if (temp < reverse) {
+        int right = 0;
+        int totalSum = sum[n - 1];
+        while(right < n){
+            int minus;
+            if (left == 0){
+                minus = 0;
+            }
+            else {
+                minus = sum[left - 1];
+            }
+            int straight = sum[right] - minus;
+            int reverse = totalSum - straight;
+            if (straight < reverse){
                 right++;
-                ans = Math.max(ans, temp);
-            } else if (temp > reverse) {
+                answer = Math.max(straight, answer);
+            }
+            else if (reverse < straight){
                 left++;
-                ans = Math.max(ans, reverse);
-            } else {
-                ans = temp;
+                answer = Math.max(answer, reverse);
+            }
+            else {
+                answer = reverse;
                 break;
             }
         }
-        System.out.println(ans);
+
+
+        System.out.println(answer);
 
     }
 }
