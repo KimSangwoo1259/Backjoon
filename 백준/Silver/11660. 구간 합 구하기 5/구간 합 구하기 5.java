52499fb608caaf64de4dd936fae4148c118d12ce
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-26 15:26:28 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 816 ms, Memory: 124276 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -2,38 +2,52 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
+    // 일단 사각형 의 부분합을 구한다.
+    // x1,y1 부터 x2,y2 까지의 부분합 점화식 sum[x2][y2] - sum[x1-1][y2] - sum[x2][y1-1] + sum[x1-1][y1-1]
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.valueOf(st.nextToken()); // 표의 크기
-        int m = Integer.valueOf(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int table[][] = new int[n+1][n+1];
-
-        int dp[][] = new int[n+1][n+1]; //누적 합 값
-
-        for (int i = 1; i < n + 1; i ++) {
+        int [][] arr = new int[n+1][n+1];
+        for (int i = 1; i <= n; i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < n + 1; j++) {
-                table[j][i] = Integer.valueOf(st.nextToken());
+            for (int j = 1; j <= n; j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 1; i < n + 1; i ++) {
-            for (int j = 1; j < n + 1; j++) {
-                dp[j][i] = dp[j - 1][i] + dp[j][i - 1] - dp[j - 1][i - 1] + table[j][i];
+        int[][] sum = new int[n+1][n+1];
+
+        sum[1][1] = arr[1][1];
+
+        for (int i = 2; i <= n; i++){
+            sum[i][1] = sum[i - 1][1] + arr[i][1];
+            sum[1][i] = sum[1][i - 1] + arr[1][i];
+        }
+
+        for (int i = 2; i <= n; i++){
+            for (int j = 2; j <= n; j++){
+                sum[i][j] = sum[i][j - 1] + sum[i - 1][j] - sum[i - 1][j - 1] + arr[i][j];
             }
         }
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i ++){
             st = new StringTokenizer(br.readLine());
-            int x1 = Integer.valueOf(st.nextToken());
-            int y1 = Integer.valueOf(st.nextToken());
-            int x2 = Integer.valueOf(st.nextToken());
-            int y2 = Integer.valueOf(st.nextToken());
-            int ans = dp[y2][x2] - dp[y1 - 1][x2] - dp[y2][x1 - 1] + dp[y1 - 1][x1 - 1];
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int ans = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
             bw.write(ans + "\n");
+
         }
         bw.flush();
         bw.close();
