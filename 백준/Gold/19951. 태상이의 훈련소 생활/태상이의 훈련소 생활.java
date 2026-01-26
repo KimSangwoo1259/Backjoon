ffa4907c235f441c4c4842b932fe8568240dcd34
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-26 16:04:05 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 652 ms, Memory: 59948 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -2,42 +2,50 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
+    // 힌트 얻고 푼 문제 이므로 얼마뒤에 한번 더 풀어보기
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.valueOf(st.nextToken()); // 연병장 크기
-        int m = Integer.valueOf(st.nextToken()); // 조교 수
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n+1];
-        int[] delta = new int[n + 1];
-        int[] sumDel = new int[n + 1];
+        int[] arr = new int[n];
+        int[] delta = new int[n];
+        int[] accDelta = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++){
-            arr[i] = Integer.valueOf(st.nextToken());
+
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int s;
-        int e;
-        int k;
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i ++){
             st = new StringTokenizer(br.readLine());
-            s = Integer.valueOf(st.nextToken());
-            e = Integer.valueOf(st.nextToken());
-            k = Integer.valueOf(st.nextToken());
-            delta[s] += k;
-            if (e != n)
-                delta[e+1] += -k;
-        }
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i <=n; i++){
-            sumDel[i] = sumDel[i - 1] + delta[i];
-            arr[i] += sumDel[i];
-            bw.write(arr[i] + " ");
+            delta[a-1] += h;
+            if (b < n){
+                delta[b] -= h;
+            }
         }
+        accDelta[0] = delta[0];
+        for (int i = 1; i < n; i ++){
+            accDelta[i] = accDelta[i - 1] + delta[i];
+        }
+        for (int i = 0; i < n; i++){
+            int temp = arr[i] + accDelta[i];
+            bw.write(temp + " ");
 
+        }
         bw.flush();
         bw.close();
+
+
     }
 }
