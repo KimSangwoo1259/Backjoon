f371a0fa603bc6e0caab45d5e414d129607e30c4
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-28 16:47:53 +0900

    [Silver I] Title: 용돈 관리, Time: 188 ms, Memory: 22420 KB -BaekjoonHub

--- 백준/Silver/6236. 용돈 관리/용돈 관리.java
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -1,47 +1,50 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    //
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken()); //인출할 횟수
 
-        int[] arr = new int[n];
-        int answer = 0;
-        
-        for (int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); // 용돈 사둉 일
+        int m = Integer.parseInt(st.nextToken()); // 인출 횟수
+        int ans = -1;
+
+        int[] money = new int[n];
+        for (int i = 0; i < n; i++) {
+            money[i] = Integer.parseInt(br.readLine());
         }
-        int left = Arrays.stream(arr).max().getAsInt();
-        int right = 2000000000;
 
-        while(left <= right){
+        int left = Arrays.stream(money).max().getAsInt();
+        int right = 1_000_000_000;
+
+        while (left <= right) {
+            int withdrawCount = 0;
             int mid = (left + right) / 2;
+            int currentMoney = 0;
+            for (int i = 0; i < n; i++) {
+                if (currentMoney < money[i]) {
+                    currentMoney = mid;
+                    withdrawCount++;
+                } 
+                currentMoney -= money[i];
+                
 
-            int cur = 0;
-            int count = 0;
-            for (int i = 0; i < n; i++){
-                if (cur < arr[i]){
-                    cur = mid;
-                    count++;
-                }
-                cur -= arr[i];
             }
-            if (count <= m){
+            if (withdrawCount <= m) { //인출을 원하는 횟수보다 적거나 같게 했다 -> 원하는거
+                ans = mid;
                 right = mid - 1;
-                answer = mid;
-            }
-            else {
+            } else {
                 left = mid + 1;
             }
 
         }
-        System.out.println(answer);
+        System.out.println(ans);
     }
+
+
 }
