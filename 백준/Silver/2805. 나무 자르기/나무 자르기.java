d3a15a77be48037ea3a28d5036a8a018d685284e
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-28 16:22:32 +0900

    [Silver II] Title: 나무 자르기, Time: 480 ms, Memory: 119396 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -1,44 +1,41 @@
 import java.io.*;
-import java.util.*;
-
+import java.util.StringTokenizer;
 
 public class Main {
 
+    //
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int answer = -1;
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[n];
+        int n = Integer.parseInt(st.nextToken()); //나무 수
+        int m = Integer.parseInt(st.nextToken()); // 가져가려는 나무 수
+        int ans = -1;
 
         st = new StringTokenizer(br.readLine());
-
+        int[] woodArr = new int[n];
         for (int i = 0; i < n; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            woodArr[i] = Integer.parseInt(st.nextToken());
         }
+
         int left = 0;
-        int right = 1000000000;
+        int right = 1_000_000_000;
 
         while(left <= right){
+            int mid = (left + right )/ 2;
             long sum = 0;
-            int mid = (left + right) / 2;
             for (int i = 0; i < n; i++){
-                sum += Math.max(0, arr[i] - mid);
+                sum += Math.max(0, woodArr[i] - mid);
             }
-            if (sum >= m){
-                answer = mid;
+            if (sum >= m) {
                 left = mid + 1;
+                ans = mid;
             }
-            else{
+            else
                 right = mid - 1;
-            }
         }
-        System.out.println(answer);
-
+        System.out.println(ans);
     }
+
 }
