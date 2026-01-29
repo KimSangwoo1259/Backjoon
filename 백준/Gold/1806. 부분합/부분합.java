40a815c889a2ebcdfe010581ad8868bd7119f70f
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-29 15:59:21 +0900

    [Gold IV] Title: 부분합, Time: 220 ms, Memory: 24008 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,42 +1,47 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
 
-        int ans = 100000001;
+        int n = Integer.parseInt(st.nextToken()); // 배열 크기
+        int s = Integer.parseInt(st.nextToken()); // 연속된 숫자 합이 s이상 이어여함
+
+        int[] arr = new int[n + 1];
         st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[n];
-        long curSum = 0;
-        for (int i = 0; i < n; i++){
+        for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int left = 0; int right = 0;
-        curSum = arr[right];
-
-        while (left < n && right < n && (left <= right)){
-            if (curSum >= s){
-                ans = Math.min(ans, right - left + 1);
-                curSum -= arr[left];
-                left++;
-            }
-            else {
-                right++;
-                if (right < n)
-                    curSum += arr[right];
-            }
+        int[] sum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + arr[i];
         }
-          if (ans == 100000001)
+
+        int ans = 100001;
+
+        int left = 1;
+        int right = 1;
+
+        while (right <= n) {
+            int temp = sum[right] - sum[left - 1];
+            if (temp >= s) {
+                int len = right - left + 1;
+                ans = Math.min(ans, len);
+                left++;
+                if (len == 1)
+                    right++;
+            } else {
+                right++;
+            }
+
+        }
+        if (ans == 100001)
             ans = 0;
         System.out.println(ans);
+
     }
 }
