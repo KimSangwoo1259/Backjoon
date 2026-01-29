473423cf48a031bce63ff36b6f6c203a821b1ebc
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-01-29 16:57:37 +0900

    [Gold IV] Title: 고냥이, Time: 188 ms, Memory: 18620 KB -BaekjoonHub

--- 백준/Gold/16472. 고냥이/고냥이.java
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -3,58 +3,44 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-        String str = br.readLine();
-        char[] arr = str.toCharArray();
-        char[] alphabet = new char[26];
-        Set<Character> set = new HashSet<>();
-        final int ASCII_VALUE = 97;
-        int left = 0;
-        int right = 0;
-        int ans = 0;
-        set.add(arr[right]);
-        alphabet[arr[right] - ASCII_VALUE]++;
-        right++;
-        boolean isRight = true;
-        while (right < arr.length){
-           if (left == right){
-               alphabet[arr[left-1]-ASCII_VALUE]--;
-               if (alphabet[arr[left-1]-ASCII_VALUE]== 0)
-                   set.remove(arr[left-1]);
-               if (set.size() <= n)
-                   ans = Math.max(right - left + 1,ans);
-               right++;
-               isRight = true;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int answer = 0;
+        Set<Character> set=new HashSet<>();
+
+        int[] count =  new int[26];
+
+        char[] strArr = br.readLine().toCharArray();
+
+        int left = 0; int right = 0;
+
+        set.add(strArr[0]);
+        count[strArr[0]-'a']++;
+        while(right < strArr.length){
+           if (set.size() <= n){
+               answer = Math.max(answer, right - left + 1);
+               right ++;
+               if (right < strArr.length){
+                   set.add(strArr[right]);
+                   count[strArr[right]-'a']++;
+               }
 
            }
-           else{
-                if(isRight){
-                    alphabet[arr[right] - ASCII_VALUE]++;
-                    set.add(arr[right]);
-                }
-                else {
-                    alphabet[arr[left-1]-ASCII_VALUE]--;
-                    if (alphabet[arr[left-1]-ASCII_VALUE] == 0)
-                        set.remove(arr[left-1]);
-                }
-               if (set.size() <= n) {
-                   ans = Math.max(right - left + 1, ans);
+           else {
+               count[strArr[left]-'a']--;
+               if (count [strArr[left]-'a'] == 0){
+                   set.remove(strArr[left]);
+               }
+               if (left == right){
                    right++;
-                   isRight = true;
                }
-               else {
-                   left++;
-                   isRight = false;
-               }
-
-
+               left++;
 
            }
         }
-        System.out.println(ans);
+        System.out.println(answer);
     }
-
-
 }
