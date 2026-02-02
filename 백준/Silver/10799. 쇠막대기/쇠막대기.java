78b9214c018eade895e44cf4507d36bee4ade0d1
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-02 14:24:57 +0900

    [Silver II] Title: 쇠막대기, Time: 128 ms, Memory: 16460 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,35 +1,42 @@
 import java.io.*;
 import java.util.Stack;
 
+
 public class Main {
+
+    // 1개의 막대기당 레이저 당한 횟수 + 1 개로 나뉘어진다 1개가 3번 레이저르 잘리면 4개가 된다,
+    // 막대기의 끝과 레이저의 ()를 어떻게 구분할 것인가?
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
-        int curStick = 0;
+        char[] stick = br.readLine().toCharArray();
+
+        int len = stick.length;
+
+        int ans = 0;
+
+        char beforeChar = ' ';
         Stack<Character> stack = new Stack<>();
 
-        for (int i = 0; i < str.length(); i++) {
-            stack.push(str.charAt(i));
-        }
-
-        int sum = 0;
-        char temp;
-        while (!stack.empty()){
-            temp = stack.pop();
-            if (temp == ')'){
-                curStick++;
-                if (stack.peek() == '('){
-                    curStick--;
-                    sum += curStick;
-                    stack.pop();
-                }
+        for (int i = 0; i < len; i++){
+            char cur = stick[i];
+            if (cur == '('){
+                stack.push(cur);
             }
             else {
-                sum++;
-                curStick--;
+                stack.pop();
+                if (beforeChar == '('){ //레이저
+                    ans += stack.size();
+                }
+                else { // 막대기의 끝
+                    ans++;
+                }
             }
+            beforeChar = cur;
         }
-        System.out.println(sum);
+
+        System.out.println(ans);
+
     }
 }
