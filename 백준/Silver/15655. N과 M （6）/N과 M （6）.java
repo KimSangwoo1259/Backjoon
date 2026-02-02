2798c14289fd69653ce7ed484791bad03b4eb6ae
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-02 21:41:30 +0900

    [Silver III] Title: N과 M (6), Time: 108 ms, Memory: 14560 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+
+
+public class Main {
+
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] ans;
+    static StringBuilder sb;
+    static boolean[] selected;
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        selected = new boolean[n];
+        ans = new int[m];
+        sb = new StringBuilder();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        recursion(0);
+        System.out.println(sb);
+    }
+
+    private static void recursion(int index) {
+        if (index == m){
+            sb.append(Arrays.stream(ans)
+                    .mapToObj(String::valueOf)
+                    .collect(Collectors.joining(" "))).append("\n");
+        }
+        else {
+            for (int i = 0; i < n; i++){
+                if (!selected[i]){
+                    if (index > 0 && ans[index - 1] > arr[i])
+                        continue;
+                    ans[index] = arr[i];
+                    selected[i]= true;
+                    recursion(index + 1);
+                    selected[i] = false;
+                }
+            }
+        }
+    }
+}
