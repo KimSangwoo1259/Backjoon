f4e4128f332336ea217e3b564e6c77596ce3d7cb
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-02 21:43:32 +0900

    [Silver III] Title: N과 M (7), Time: 828 ms, Memory: 299068 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int m;
+    static Integer[] arr;
+    static Integer[] ans;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.valueOf(st.nextToken());
+        m = Integer.valueOf(st.nextToken());
+
+        arr = new Integer[n];
+        ans = new Integer[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        back(0);
+        bw.flush();
+        bw.close();
+
+    }
+
+    static void back(int num) throws IOException {
+
+        if (num == m){
+            for (int i = 0; i < m; i++){
+                bw.write(ans[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++){
+
+            ans[num] = arr[i];
+            back(num+1);
+        }
+    }
+
+
+}
