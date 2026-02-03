1f090d103bbbdca9215f91f2bc62df76fb0180ce
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-03 15:12:18 +0900

    [Silver II] Title: 부분수열의 합, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,41 +1,42 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
 
 public class Main {
+
+
+    // 수열이므로 연속 될 필요는 없음. 총 원소 개수가 20 이므로 2^20 가지 이므로 총 가짓수 about 100000 이므로 브루투 포스 알고리즘으로 가능
+
     static int n;
     static int s;
-    static int ans;
     static int[] arr;
-    static int[] temp;
-
+    static int ans;
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.valueOf(st.nextToken());
-        s = Integer.valueOf(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
         ans = 0;
         arr = new int[n];
-        temp = new int[n];
+
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++){
-            arr[i] = Integer.valueOf(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        back(0,0);
-        ans = (s == 0) ? ans - 1 : ans;
+        backtrack(0,0);
         System.out.println(ans);
 
     }
-    static void back(int num, int sum){
-        if (num == n){
-            if (sum == s)
-                ans++;
-
+    static void backtrack(int i, int sum){
+        if (i == n)
             return;
-        }
-        back(num + 1, sum + arr[num]);
-        back(num +1 , sum);
-
+        if (sum + arr[i] == s)
+            ans++;
+        backtrack(i+1,sum);
+        backtrack(i+1,sum + arr[i]);
     }
 }
