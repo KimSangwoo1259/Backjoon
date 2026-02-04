49bd90d7e2eb5072966c2ea0919545cd2af0fc70
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-04 20:58:44 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 464 ms, Memory: 116672 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,47 +1,53 @@
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
     static int n;
     static int m;
     static int[][] graph;
     static boolean[] visited;
-    static int count;
+    static int ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.valueOf(st.nextToken());
-        m = Integer.valueOf(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
         graph = new int[n + 1][n + 1];
         visited = new boolean[n + 1];
-        count = 0;
-        int a;
-        int b;
-        for (int i = 1; i <= m; i++){
+        ans = 0;
+
+        for (int i = 0; i < m; i++){
             st = new StringTokenizer(br.readLine());
-            a = Integer.valueOf(st.nextToken());
-            b = Integer.valueOf(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
             graph[a][b] = 1;
             graph[b][a] = 1;
         }
-        for (int i = 1; i <=n; i++){
-            if (!visited[i]){
-                dfs(i);
-                count++;
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i =1; i <= n; i ++){
+            if (visited[i])
+                continue;
+            ans++;
+            q.add(i);
+            visited[i] = true;
+            while(!q.isEmpty()){
+                int now = q.poll();
+                for (int j = 1; j <=n; j++){
+                    if (!visited[j] && graph[now][j] == 1){
+                        q.add(j);
+                        visited[j] =true;
+                    }
+                }
             }
         }
-        System.out.println(count);
-    }
-    static void dfs(int node){
-        visited[node] = true;
-        for (int i = 1; i <= n; i++){
-            if (graph[node][i] == 1 && !visited[i]){
-                dfs(i);
-            }
-        }
+        System.out.println(ans);
+
     }
 }
