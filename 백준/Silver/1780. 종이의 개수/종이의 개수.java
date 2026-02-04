264921cd72ec9c65bf9e33aabca3d97654bc8a7d
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-04 18:07:31 +0900

    [Silver II] Title: 종이의 개수, Time: 864 ms, Memory: 310516 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.java
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.java
@@ -2,65 +2,59 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] ans;
-    static int[][] arr;
+
+
     static int n;
+    static int[][] paper;
+    static int[] ans;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.valueOf(br.readLine());
+        n = Integer.parseInt(br.readLine());
         ans = new int[3];
-        arr = new int[n][n];
+        paper = new int[n][n];
 
-        StringTokenizer st;
         for (int i = 0; i < n; i++){
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++){
-                arr[i][j] = Integer.valueOf(st.nextToken());
+                paper[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        cut(0, 0, n);
-        for (int i = 0; i < 3; i++){
-            System.out.println(ans[i]);
-        }
 
-
+        recursion(n,0,0);
+        System.out.println(ans[0] + "\n" + ans[1] + "\n" + ans[2]);
     }
-    static void cut(int x, int y, int dif){
-        if (dif == 1){
-            ans[arr[x][y]+ 1]++;
+    static void recursion(int len, int x, int y){
+        int firstValue = paper[x][y];
+
+        if (len == 1){
+            ans[firstValue + 1]++;
             return;
         }
-        int temp = arr[x][y];
 
-        boolean isAllSame = true;
-
-        for (int i = x; i < x + dif; i++){
-            for (int j = y; j < y + dif; j++){
-                if (temp != arr[i][j]){
-                    isAllSame = false;
+        boolean isSame = true;
+        for (int i = x; i < x +len; i++){
+            for (int j = y; j < y + len; j++){
+                if (firstValue != paper[i][j]){
+                    isSame= false;
                     break;
                 }
+                if (!isSame)
+                    break;
+
             }
-            if (!isAllSame)
-                break;
         }
-        if (isAllSame){
-                ans[temp+1]++;
+        if (isSame){
+            ans[firstValue + 1]++;
             return;
         }
-        else {
-            int nextDif = dif / 3;
-            cut(x,y,nextDif);
-            cut(x,y+nextDif,nextDif);
-            cut(x, y + nextDif * 2, nextDif);
-            cut(x+nextDif,y,nextDif);
-            cut(x+nextDif,y+nextDif,nextDif);
-            cut(x+nextDif, y + nextDif * 2, nextDif);
-            cut(x+nextDif*2,y,nextDif);
-            cut(x+nextDif*2,y+nextDif,nextDif);
-            cut(x+nextDif*2, y + nextDif * 2, nextDif);
+        for (int i = x; i < x + len; i += (len / 3)){
+            for (int j = y; j < y + len; j += (len / 3)){
+                recursion(len/3,i,j);
+            }
         }
+
     }
+
 
 }
