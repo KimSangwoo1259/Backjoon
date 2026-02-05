0bc61d847ddcf7b281b522177d7791307994f5f5
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-05 22:14:28 +0900

    [Gold V] Title: 토마토, Time: 588 ms, Memory: 120948 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -4,79 +4,78 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] preTomato;
-    static int[][] tomato;
-    static boolean[][] visited;
-    static int m;
-    static int n;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-    static int ans = 0;
-    static int count = 0;
-    static int ansTime = 0;
+
+
+    static class Node {
+        int x;
+        int y;
+        int time;
+        public Node(int x, int y, int time) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int minus = 0;
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.valueOf(st.nextToken());
-        n = Integer.valueOf(st.nextToken());
-        tomato = new int[n][m];
-        preTomato = new int[n][m];
-        visited = new boolean[n][m];
-        Queue<Tomato> q = new LinkedList<>();
+
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] box = new int[n][m];
+
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+        int target = n * m;
+        int curCount = 0;
+        int time = 0;
+        Queue<Node> q = new LinkedList<>();
         for (int i = 0; i < n; i++){
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++){
-                tomato[i][j] = Integer.valueOf(st.nextToken());
-                if (tomato[i][j] == -1)
-                    minus++;
-                if (tomato[i][j] == 1){
-                    q.add(new Tomato(i, j,0));
-                    visited[i][j] = true;
-                    count++;
+                int value = Integer.parseInt(st.nextToken());
+                box[i][j] = value;
+                if (value == -1)
+                    target--;
+                else if (value == 1) {
+                    curCount++;
+                    q.add(new Node(i, j, 0));
                 }
             }
         }
-        ans = n * m - minus; // 익을 수 있는 총 토마토 개수
-        preTomato = tomato;
-        if (count == ans){
-            System.out.println(ansTime);
-            System.exit(0);
+        if (target == curCount){
+            System.out.println(0);
+            return;
         }
 
-
-        while (!q.isEmpty()){
-            Tomato now = q.poll();
+        while(!q.isEmpty()){
+            Node now = q.poll();
             for (int i = 0; i < 4; i++){
-                int nextX = now.x + dr[i];
-                int nextY = now.y + dc[i];
-                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m || tomato[nextX][nextY] == -1) continue;
+                int nx = now.x + dr[i];
+                int ny = now.y + dc[i];
 
-                if (!visited[nextX][nextY]){
-                    count++;
-                    visited[nextX][nextY] = true;
-                    q.add(new Tomato(nextX, nextY, now.time + 1));
-                    ansTime = Math.max(ansTime, now.time + 1);
+                if (nx >= 0 && nx < n && ny >= 0 && ny< m && box[nx][ny] == 0){
+                    q.add(new Node(nx, ny, now.time + 1));
+                    box[nx][ny] = 1;
+                    curCount++;
+                    if (curCount == target){
+                        time = now.time + 1;
+                        break;
+                    }
                 }
             }
         }
-        if (count != ans){
+
+        if (curCount == target) {
+            System.out.println(time);
+        }
+
+        else
             System.out.println(-1);
-        }
-        else {
-            System.out.println(ansTime);
-        }
 
-    }
-}
-// 전 토마토 상태를 저장해놓고 그 기준으로 토마토 전파, 처음에 총 개수를 파악해야함
-
-class Tomato {
-    int x,y,time;
-    Tomato(int x, int y, int time){
-        this.x = x;
-        this.y = y;
-        this.time = time;
     }
 }
