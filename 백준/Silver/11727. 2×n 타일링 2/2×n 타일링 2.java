985b5751f1be04268ce7dc6cc313322516bbb8dc
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-08 19:35:50 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,20 +1,12 @@
-import java.io.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-
-        long[] dp = new long[n + 1];
-
-        dp[1] = 1;
-        if (n >= 2) {
-            dp[2] = 3;
-            for (int i = 3; i <= n; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
-            }
-        }
-        System.out.println(dp[n]);
-
-    }
-}
+<html>
+<head><title>504 Gateway Time-out</title></head>
+<body>
+<center><h1>504 Gateway Time-out</h1></center>
+</body>
+</html>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
