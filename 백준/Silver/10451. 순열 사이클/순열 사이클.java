0f12a5b7f8a2ca6a9bc839209a85021e1eb84c57
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-09 22:59:47 +0900

    [Silver III] Title: 순열 사이클, Time: 408 ms, Memory: 52408 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -1,41 +1,50 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
     static int n;
     static int[] graph;
     static boolean[] visited;
+    static int ans;
+    //분명 왔단길을 다시 돌아갈 수 밖에 없음.. 그렇다고 visited 관리를 안하면 관리가 안되는데.. 가진 열쇠를 값으로 바꾸어서 visited 활용
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.valueOf(br.readLine());
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        while (t --> 0){
-            n = Integer.valueOf(br.readLine());
-            st = new StringTokenizer(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+        List<Integer>[] a = new List[n+1];
 
-            graph = new int[n+1];
+        while(t--> 0){
+            ans = 0;
+            n = Integer.parseInt(br.readLine());
+            graph = new int[n + 1];
             visited = new boolean[n + 1];
-            int count = 0;
-            for (int i = 1; i <= n; i++){
-                graph[i] = Integer.valueOf(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i = 1; i<=n; i++){
+                graph[i] = Integer.parseInt(st.nextToken());
             }
-            for (int i = 1; i <=n; i++){
+            for (int i = 1; i <= n; i++){
                 if (!visited[i]){
+                    ans++;
                     dfs(i);
-                    count++;
                 }
             }
-            bw.write(count + "\n");
+            bw.write(ans + "\n");
+
         }
         bw.flush();
         bw.close();
-    }
-    static void dfs(int node) {
-        visited[node] = true;
-        if (!visited[graph[node]])
-            dfs(graph[node]);
+
 
     }
+    static void dfs(int l){
+        if (!visited[graph[l]]){
+            visited[graph[l]] = true;
+            dfs(graph[l]);
+        }
+    }
+
+
 }
+
