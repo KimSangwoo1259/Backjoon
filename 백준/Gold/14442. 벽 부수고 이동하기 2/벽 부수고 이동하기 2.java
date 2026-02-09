9d5f6f143632e758ac49ed84463e9f18f90fdfeb
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-09 18:55:01 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1304 ms, Memory: 377264 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,76 +1,90 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static int k;
-    static int[][] map;
-    static int[][][] visited;
-    static Queue<Person> q;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+
+
+
+    static class Person {
+        int x;
+        int y;
+        int time;
+        int count;
+
+        public Person(int x, int y, int time, int count) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+            this.count = count;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.valueOf(st.nextToken());
-        m = Integer.valueOf(st.nextToken());
-        k = Integer.valueOf(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
 
+        boolean success = false;
+        int ans = 0;
 
-        map = new int[n][m];
-        visited = new int[n][m][k+1];
+        int[][] map = new int[n][m];
+        boolean[][][] visited = new boolean[n][m][k + 1];
 
         for (int i = 0; i < n; i++){
-            String temp = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++){
-                map[i][j] = temp.charAt(j) - '0';
+                map[i][j] = Character.getNumericValue(line.charAt(j));
             }
         }
 
-        q = new LinkedList<>();
-        q.add(new Person(0, 0, 0));
-        visited[0][0][0] = 1;
-        while (!q.isEmpty()){
+        visited[0][0][0] = true;
+        Queue<Person> q = new LinkedList<>();
+        q.add(new Person(0, 0, 1, 0));
+
+        while(!q.isEmpty()){
             Person now = q.poll();
-            if (now.x == n - 1 && now.y == m - 1){
-                System.out.println(visited[now.x][now.y][now.wall]);
-                return;
+
+            if (now.x == n -1 && now.y == m-1){
+                ans = now.time;
+                success = true;
+                break;
             }
-            for (int i = 0; i < 4; i++){
-                int nX = now.x + dr[i];
-                int nY = now.y + dc[i];
-                if (nX < 0 || nX >= n || nY <0 || nY >= m)
-                    continue;
-                if (visited[nX][nY][now.wall] == 0){
-                    if (map[nX][nY] == 0){
-                        visited[nX][nY][now.wall] = visited[now.x][now.y][now.wall] + 1;
-                        q.add(new Person(nX, nY, now.wall));
+            for (int i = 0; i < 4; i ++){
+                int nx = now.x + dr[i];
+                int ny = now.y + dc[i];
+
+                if (nx >= 0 && nx < n && ny >=0 && ny < m){
+                    int value = map[nx][ny];
+
+                    if (value == 0){
+                        if (!visited[nx][ny][now.count]) {
+                            visited[nx][ny][now.count] = true;
+                            q.add(new Person(nx, ny, now.time + 1, now.count));
+                        }
                     }
-                    else if(map[nX][nY] == 1 && now.wall < k && visited[nX][nY][now.wall+ 1] == 0){
-                        visited[nX][nY][now.wall + 1] = visited[now.x][now.y][now.wall] + 1;
-                        q.add(new Person(nX, nY, now.wall + 1));
+                    else { // 벽인 경우
+                        if (now.count < k && !visited[nx][ny][now.count + 1]){
+                            visited[nx][ny][now.count + 1] = true;
+                            q.add(new Person(nx, ny, now.time + 1, now.count + 1));
+                        }
+
                     }
+
                 }
             }
-        }
 
+        }
+        if (success)
+            System.out.println(ans);
+        else
             System.out.println(-1);
-            return;
-        }
-
-    static class Person{
-        int x,y,wall;
-        Person(int x, int y,int wall){
-            this.x = x;
-            this.y = y;
-            this.wall = wall;
-        }
     }
 }
