1cde9f2669de5b9d457dbc9985882261f12c0c8b
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-09 18:41:09 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 680 ms, Memory: 125592 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -4,74 +4,92 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] map;
-    static int[][][] visited;
-    static Queue<Person> q;
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+
+
+
+    static class Person {
+        int x;
+        int y;
+        int time;
+        int remain;
+
+        public Person(int x, int y, int time, int remain) {
+            this.x = x;
+            this.y = y;
+            this.time = time;
+            this.remain = remain;
+        }
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        n = Integer.valueOf(st.nextToken());
-        m = Integer.valueOf(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
 
-        map = new int[n][m];
-        visited = new int[n][m][2];
+        boolean success = false;
+        int ans = 0;
+
+        int[][] map = new int[n][m];
+        boolean[][] visited = new boolean[n][m];
+        boolean[][] visitedWithBreak = new boolean[n][m];
 
         for (int i = 0; i < n; i++){
-            String temp = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++){
-                map[i][j] = temp.charAt(j) - '0';
+                map[i][j] = Character.getNumericValue(line.charAt(j));
             }
         }
 
-        q = new LinkedList<>();
-        q.add(new Person(0, 0, 0));
-        visited[0][0][0] = 1;
-        while (!q.isEmpty()){
+        visited[0][0] = true;
+        Queue<Person> q = new LinkedList<>();
+        q.add(new Person(0, 0, 1, 1));
+
+        while(!q.isEmpty()){
             Person now = q.poll();
-            if (now.x == n - 1 && now.y == m - 1){
-                System.out.println(visited[now.x][now.y][now.wall]);
-                return;
+
+            if (now.x == n -1 && now.y == m-1){
+                ans = now.time;
+                success = true;
+                break;
             }
             for (int i = 0; i < 4; i++){
-                int nX = now.x + dr[i];
-                int nY = now.y + dc[i];
-                if (nX < 0 || nX >= n || nY <0 || nY >= m)
-                    continue;
-                if (visited[nX][nY][now.wall] == 0){
-                    if (map[nX][nY] == 0){
-                        visited[nX][nY][now.wall] = visited[now.x][now.y][now.wall] + 1;
-                        q.add(new Person(nX, nY, now.wall));
-                    }
-                    else {
-                        if (now.wall == 0){
-                            visited[nX][nY][now.wall + 1] = visited[now.x][now.y][now.wall] + 1;
-                            q.add(new Person(nX, nY, 1));
+                int nx = now.x + dr[i];
+                int ny = now.y + dc[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m){
+                    if (now.remain == 1){ // 벽 부술 수 있음
+                        if (map[nx][ny] == 0 && !visited[nx][ny]){
+                            visited[nx][ny] = true;
+                            visitedWithBreak[nx][ny] = true;
+
+                            q.add(new Person(nx, ny, now.time + 1, now.remain));
                         }
+                        else { //벽으로
+                            if (!visitedWithBreak[nx][ny]) {
+                                visitedWithBreak[nx][ny] = true;
+                                q.add(new Person(nx, ny, now.time + 1, 0));
+                            }
+                        }
+                    }
+                    else { // 벽 부술 수 없음
+                        if (map[nx][ny] == 0 && !visitedWithBreak[nx][ny]){
+                            visitedWithBreak[nx][ny] = true;
+                            q.add(new Person(nx, ny, now.time + 1, now.remain));
+                        }
+
                     }
                 }
             }
 
-
         }
-        System.out.println(-1);
-        return;
-
-
-
-
-    }
-    static class Person{
-        int x,y,wall;
-        Person(int x, int y,int wall){
-            this.x = x;
-            this.y = y;
-            this.wall = wall;
-        }
+        if (success)
+            System.out.println(ans);
+        else
+            System.out.println(-1);
     }
 }
