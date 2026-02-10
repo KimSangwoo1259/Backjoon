b97f5f446692d9a8bd1c0914d4e2bfa755ab0f1a
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-10 16:05:00 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 16344 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,26 +1,41 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.valueOf(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        int alc[] = new int[n + 1];
-        int dp[] = new int[n + 1];
-
-        for (int i = 1; i <= n; i++) {
-            alc[i] = Integer.valueOf(br.readLine());
-
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        dp[1] = alc[1];
-        if(n>= 2)
-            dp[2] = alc[1] + alc[2];
-        for (int i = 3; i <=n; i++){
-            dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2], dp[i - 3] + alc[i - 1]) + alc[i]); // 계단 오르기와는 다르게 지금 와인을 마시지 않는 경우도 고려해야함
+        int[][] dp = new int[n][3]; // 0 -> 안먹음 1-> 처음으로 먹음 2-> 연속 두번째로 먹은거임
+        // dp[n][0] = dp[n-1] 값 중에 maxxa dp[n][1] = dp[n-1][0] + arr[n] dp[n][2] = dp[n-1][1] + arr[i]
+        dp[0][1] = arr[0];
+        if (n > 1){
+            dp[1][0] = arr[0];
+            dp[1][1] = arr[1];
+            dp[1][2] = dp[0][1] + arr[1];
+
+            for (int i = 2; i < n; i++){
+                dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
+                dp[i][1] = dp[i - 1][0] + arr[i];
+                dp[i][2] = dp[i - 1][1] + arr[i];
+            }
         }
-        System.out.println(dp[n]);
+
+        int ans = Arrays.stream(dp[n - 1]).max().getAsInt();
+
+        System.out.println(ans);
 
 
     }
+
+
 }
+
