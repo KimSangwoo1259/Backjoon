95f29524673f0788bf515355a393f5111cdd50de
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-11 20:41:34 +0900

    [Silver I] Title: 동물원, Time: 120 ms, Memory: 17676 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.java
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -1,22 +1,31 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.valueOf(br.readLine());
-        long dp[][] = new long[n + 1][3]; //[n][0] -> 두칸다 없는경우, [n][1] -> 왼쪽만 있는경우, [n][2] -> 오른쪽만 있는경우
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        final int MOD = 9901;
+        int[][] dp = new int[n + 1][3]; //0 없음 1 왼쪽 2 오른쪽
+        // dp[n][0] = dp[n-1][0] + dp[n-1][1] + dp[n-1] + [2]
         dp[1][0] = 1; dp[1][1] = 1; dp[1][2] = 1;
-        int mod = 9901;
         for (int i = 2; i <= n; i++){
-            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2])  % mod;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
-            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % mod;
+            dp[i][0] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % MOD;
+            dp[i][1] = (dp[i - 1][0] + dp[i-1][2]) % MOD;
+            dp[i][2] = (dp[i - 1][0] + dp[i - 1][1]) % MOD;
         }
 
-        System.out.println((dp[n][0] + dp[n][1] + dp[n][2]) % mod);
+        int ans = Arrays.stream(dp[n]).sum() % MOD;
 
-
+        System.out.println(ans);
 
     }
+
 }
+// 배치 가능경우, 왼쪽, 오른쪽, x
+
