be485e0c6b0b424da028ac9fea19d6a01f390062
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-11 21:38:49 +0900

    [Silver I] Title: RGB거리, Time: 124 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -3,34 +3,34 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.valueOf(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[][] house = new int[n + 1][3];
 
+        for (int i = 1; i <= n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++){
+                house[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] dp = new int[n + 1][3];
 
-        int cost[][] = new int[n+1][3]; //칠하는 비용
-        int dp[][] = new int[n + 1][3];
         for (int i = 1; i <=n; i++){
-            st = new StringTokenizer(br.readLine());
-            cost[i][0] = Integer.valueOf(st.nextToken());
-            cost[i][1] = Integer.valueOf(st.nextToken());
-            cost[i][2] = Integer.valueOf(st.nextToken());
+            dp[i][0] += Math.min(dp[i - 1][1], dp[i - 1][2]) + house[i][0];
+            dp[i][1] += Math.min(dp[i - 1][0], dp[i - 1][2]) + house[i][1];
+            dp[i][2] += Math.min(dp[i - 1][1], dp[i - 1][0]) + house[i][2];
         }
-        dp[1][0] = cost[1][0];
-        dp[1][1] = cost[1][1];
-        dp[1][2] = cost[1][2];
-        for (int i = 2; i <=n; i++){
-            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + cost[i][0];
-            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + cost[i][1];
-            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + cost[i][2];
 
-        }
-        int min = Arrays.stream(dp[n]).min().getAsInt(); //마지막 열에서 최소값을 찾으면 정답
+        System.out.println(Arrays.stream(dp[n]).min().getAsInt());
 
-        System.out.println(min);
 
 
     }
+
 }
+
