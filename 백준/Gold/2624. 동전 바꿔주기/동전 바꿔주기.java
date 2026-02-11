8ed4ddd091617fa905438d64c3e844f3caa140b3
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-11 15:52:29 +0900

    [Gold IV] Title: 동전 바꿔주기, Time: 136 ms, Memory: 16496 KB -BaekjoonHub

--- 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
+++ 백준/Gold/2624. 동전 바꿔주기/동전 바꿔주기.java
@@ -1,41 +1,43 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.StringTokenizer;
+import java.util.*;
 
-/**
- * 20 = 10×2
- * 20 = 10×1 + 5×2
- * 20 = 10×1 + 5×1 + 1×5
- * 20 = 5×3 + 1×5
- * 입력으로 지폐의 금액 T, 동전의 가지 수 k, 각 동전 하나의 금액 pi와 개수 ni가 주어질 때 (i=1, 2,…, k)
- * 지폐를 동전으로 교환하는 방법의 가지 수를 계산하는 프로그램을 작성하시오. 방법의 수는 231-1을 초과 하지 않는 것으로 가정한다.
- */
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
 
-        int t = Integer.valueOf(br.readLine()); // 지폐의 금액
-        int k = Integer.valueOf(br.readLine()); // 가지수
-
-        int coin[][] = new int[k+1][2]; // [k][0] = 동전 가격 [k][1] = 동전 개수
-
-        for (int i = 1; i <= k; i++){
-            st = new StringTokenizer(br.readLine());
-            coin[i][0] = Integer.valueOf(st.nextToken());
-            coin[i][1] = Integer.valueOf(st.nextToken());
-        }
-        int dp[][] = new int[k+1][t+1];
+        int[][] coin = new int[k+1][2];
+        int[][] dp = new int[k+1][t + 1]; //dp[k][t] =  k번째 동전까지 사용했을때 t원을 만들 수 있는 경우의 수
+        //만약 k 번째 동전의 가치가 v 라면 dp[k][t+v] += dp[k-1][t] 가 된다
         dp[0][0] = 1;
         for (int i = 1; i <= k; i++){
-            for (int j = 0; j<=coin[i][1]; j++){
-                for (int l = 0+(j*coin[i][0]); l <=t; l++){
-                    dp[i][l] += dp[i - 1][l - (j * coin[i][0])];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int index = Integer.parseInt(st.nextToken());
+            int quantity = Integer.parseInt(st.nextToken());
+
+            coin[i][0] = index;
+            coin[i][1] = quantity;
+        }
+        for (int i = 1; i <=k; i++){
+            int coinPrice = coin[i][0];
+            int coinCount = coin[i][1];
+            for (int value = 0; value <=t; value++){
+                for (int cnt = 0; cnt <= coinCount; cnt++){
+                    int nextValue = value + cnt * coinPrice;
+                    if (nextValue > t) break;
+                    dp[i][nextValue] += dp[i - 1][value];
                 }
             }
         }
         System.out.println(dp[k][t]);
+
     }
+
+
 }
+
