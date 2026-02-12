bd9c3e766ae654ecc14dc22129bcba7fa649e117
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-12 19:21:07 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 26476 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,67 +1,36 @@
 import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
+
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
-        MaxPriorityQueue pq = new MaxPriorityQueue();
-        int n = Integer.valueOf(br.readLine());
-        while(n --> 0){
-            int x = Integer.valueOf(br.readLine());
-            if (x == 0){
-                bw.write(pq.pop()+ "\n");
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++){
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0){
+                if (pq.isEmpty())
+                    sb.append(0);
+
+                else
+                    sb.append(pq.poll());
+                sb.append("\n");
             }
-            else
-                pq.push(x);
-        }
-        bw.flush();
-        bw.close();
-    }
-}
-
-class MaxPriorityQueue{
-    int[] heap;
-    int size;
-    public MaxPriorityQueue(){
-        heap = new int[100001];
-        size = 0;
-    }
-    void swap(int a, int b){
-        int tmp = heap[a];
-        heap[a] = heap[b];
-        heap[b] = tmp;
-    }
-
-    public void push(int x) {
-        heap[++size] = x;
-        int current = size;
-        while (current > 1){
-            int parent = current / 2;
-            if(heap[parent] >= heap[current]) break;
-            swap(parent, current);
-            current = parent;
-        }
-    }
-    public int pop() {
-        if (size == 0) return 0;
-        int ret = heap[1];
-
-        heap[1] = heap[size--];
-        int current = 1;
-        while (current * 2 <= size){
-            int left = current * 2;
-            int right = left + 1;
-            int child = left;
-            if (right <= size && heap[left] < heap[right]){
-                child = right;
+            else {
+                pq.add(num);
             }
-            if (heap[current] >= heap[child]) break;
-            swap(current, child);
-            current = child;
         }
 
+        System.out.println(sb);
+        
+     }
 
-        return ret;
-    }
 }
