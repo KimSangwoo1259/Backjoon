60d7c76a049aaa903592ecfb410fe2ae83811425
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-19 23:14:03 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 480 ms, Memory: 52728 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,42 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    static class Edge {
+        int dist;
+        int cost;
+
+        public Edge(int dist, int cost) {
+            this.dist = dist;
+            this.cost = cost;
+        }
+    }
+
+    // 먼저푸는것이 좋은문제가 있다 -> 그거먼저, 근데 그게 없다? 그러면 쉬운문제 부터 풀기.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.valueOf(br.readLine());
-        int m = Integer.valueOf(br.readLine());
-        int INF = 1000000000;
-        List<Edge>[] graph = new List[n + 1];
+
+        int n = Integer.parseInt(br.readLine());
+        final int INF = 2_000_000_000;
         int[] cost = new int[n + 1];
         int[] path = new int[n + 1];
-
-        for (int i = 0; i <= n; i++){
+        List<Edge>[] list = new List[n + 1];
+        for (int i = 1; i <=n; i++){
+            list[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <=n; i++){
             cost[i] = INF;
-            graph[i] = new ArrayList<>();
-        }
 
-        for (int i = 1; i <= m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.valueOf(st.nextToken());
-            int b = Integer.valueOf(st.nextToken());
-            int c = Integer.valueOf(st.nextToken());
-            graph[a].add(new Edge(b, c));
         }
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.valueOf(st.nextToken());
-        int end = Integer.valueOf(st.nextToken());
+        boolean[] visited = new boolean[n + 1];
+
+        int m = Integer.parseInt(br.readLine());
+
+        while(m --> 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            list[s].add(new Edge(e, c));
+        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int start = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
+
+        cost[start] = 0;
         PriorityQueue<Edge> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.cost));
         pq.offer(new Edge(start, 0));
-        cost[start] = 0;
 
-        while (!pq.isEmpty()){
+        while(!pq.isEmpty()){
             Edge now = pq.poll();
-            if (cost[now.dist] < now.cost) continue;
-            for (Edge next: graph[now.dist]){
+            if (cost[now.dist] < now.cost)  continue;
+            for (Edge next: list[now.dist]){
                 if (cost[next.dist] > cost[now.dist] + next.cost){
                     cost[next.dist] = cost[now.dist] + next.cost;
                     pq.offer(new Edge(next.dist, cost[next.dist]));
@@ -44,29 +60,23 @@ public class Main {
                 }
             }
         }
-        System.out.println(cost[end]);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(cost[target]).append("\n");
         Stack<Integer> stack = new Stack<>();
-        int now = end;
-        while (now != 0){
-            stack.add(now);
+
+        int now = target;
+        while(now != 0){
+            stack.push(now);
             now = path[now];
         }
-        System.out.println(stack.size());
-
-        while (!stack.empty()){
-            int tep = stack.pop();
-            System.out.print(tep + " ");
-
+        sb.append(stack.size()).append("\n");
+        while(!stack.isEmpty()){
+            sb.append(stack.pop()).append(" ");
         }
+        System.out.println(sb);
 
 
     }
 }
-class Edge{
-    int dist;
-    int cost;
-    Edge(int d, int c) {
-        dist = d;
-        cost = c;
-    }
-}
+
