28da5f505fdbb3f800b2d03bcaed25fd767c94e7
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-19 22:36:31 +0900

    [Gold V] Title: 최소비용 구하기, Time: 440 ms, Memory: 46908 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,59 +1,61 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+
+
+    // 먼저푸는것이 좋은문제가 있다 -> 그거먼저, 근데 그게 없다? 그러면 쉬운문제 부터 풀기.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int n = Integer.valueOf(br.readLine());
-        int m = Integer.valueOf(br.readLine());
-        int INF = 1000000000;
-        int[][] graph = new int[n + 1][n + 1];
-        int[] cost = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
 
-        for (int i = 0; i <= n; i++){
+        int n = Integer.parseInt(br.readLine());
+        final int INF = 2_000_000_000;
+        int[] cost = new int[n + 1];
+        int[][] graph = new int[n + 1][n + 1];
+        for (int i = 1; i <=n; i++){
             cost[i] = INF;
-            for (int j = 0; j <=n; j++){
+            for (int j = 1; j <=n; j++){
                 graph[i][j] = INF;
             }
         }
+        boolean[] visited = new boolean[n + 1];
 
-        for (int i = 1; i <= m; i++){
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.valueOf(st.nextToken());
-            int b = Integer.valueOf(st.nextToken());
-            int c = Integer.valueOf(st.nextToken());
-            if (graph[a][b] > c)
-                graph[a][b] = c;
+        int m = Integer.parseInt(br.readLine());
+
+        while(m --> 0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (graph[s][e] > c)
+                graph[s][e] = c;
         }
-        st = new StringTokenizer(br.readLine());
-        int start = Integer.valueOf(st.nextToken());
-        int end = Integer.valueOf(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int start = Integer.parseInt(st.nextToken());
+        int target = Integer.parseInt(st.nextToken());
         cost[start] = 0;
         for (int i = 1; i <=n; i++){
             int min = INF;
             int minIndex = -1;
-            for (int j = 1; j <=n; j++){
+            for (int j = 1; j <= n; j++){
                 if (cost[j] < min && !visited[j]){
                     min = cost[j];
                     minIndex = j;
                 }
             }
-            if (minIndex == -1) break;
+            if (minIndex == -1)
+                break;
             visited[minIndex] = true;
-            for (int j = 1; j <= n; j++){
-                if (cost[j] > cost[minIndex] + graph[minIndex][j])
-                    cost[j] = cost[minIndex] + graph[minIndex][j];
+            for (int j = 1; j <=n; j++){
+                cost[j] = Math.min(cost[j], cost[minIndex] + graph[minIndex][j]);
             }
-
         }
-        System.out.println(cost[end]);
 
-
-
+        System.out.println(cost[target]);
 
 
     }
 }
+
