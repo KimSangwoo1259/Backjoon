3973b32a747741914a9b9de6edeaca35cbb5f7f9
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-02-20 19:36:08 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 868 ms, Memory: 26996 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -1,46 +1,56 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.valueOf(st.nextToken());
-        int m = Integer.valueOf(st.nextToken());
-
-
-        int square[][] = new int[n+1][m+1];
-        int dp[][] = new int[n+1][m+1];
-        for (int i = 1; i <= n; i++){
-            String s = br.readLine();
-            for (int j = 1; j <= m; j++){
-                square[i][j] = Character.getNumericValue(s.charAt(j-1));
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int max = 0;
+        int[][] arr = new int[n + 1][m + 1];
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 1; i<=n; i++){
+            String str = br.readLine();
+            for (int j = 1; j <=m; j++){
+                int value = Character.getNumericValue(str.charAt(j - 1));
+                arr[i][j] = value;
+                dp[i][j] = value;
+                max = Math.max(max, value);
             }
         }
-        int max = 0;
-        for (int i = 1; i <=n; i++){
-            for (int j = 1; j <=m; j++){
-                if(square[i][j] == 1){
-                    dp[i][j] = 1;
-                    if (dp[i-1][j-1] >= 1){
-                        for (int k = 1; k <= dp[i-1][j-1]; k++){
-                            if (square[i-k][j] <1 || square[i][j-k] < 1){
+        for (int i = 1; i <=n-1; i++){
+            for (int j = 1; j <=m-1; j++){
+                int len = dp[i][j];
+                if (len >= 1){
+                    if (arr[i+1][j+1] == 1){
+                        int temp = 0;
+                        for (int k = 0; k < len; k++){
+                            if (arr[i+1][j - k] == 1 && arr[i - k][j + 1] == 1){
+                                temp++;
+                            }
+                            else{
                                 break;
                             }
-                            else {
-                                dp[i][j]++;
-                            }
+
                         }
 
+                        dp[i + 1][j + 1] = 1 + temp;
+                        max = Math.max(max, dp[i + 1][j + 1]);
+
                     }
-                    max = Math.max(dp[i][j], max);
                 }
             }
         }
+        
         System.out.println(max * max);
 
+
     }
+
 }
+
