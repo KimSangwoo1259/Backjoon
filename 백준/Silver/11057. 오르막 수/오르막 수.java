edfb6f9ec9fcfb190b5576794b18342a5abac626
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-03-03 15:07:31 +0900

    [Silver I] Title: 오르막 수, Time: 104 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11057. 오르막 수/오르막 수.java
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -1,30 +1,34 @@
 import java.io.*;
+import java.util.Arrays;
+
 
 public class Main {
+
+    //1자리 0 ~ 9
+    //2자리 00 ~ 09 11 ~ 19 ...
+    // 001 002 ~ 099 111 199
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.valueOf(br.readLine());
-        long dp[][] = new long[1001][10];
+        final int MOD = 10007;
 
-        for (int i = 0; i < 10; i ++){
+        int[][] dp = new int[n + 1][10];
+
+        for (int i = 0; i < 10; i++){
             dp[1][i] = 1;
         }
-        for (int i = 2; i <= n; i++){
-            for (int j = 0; j <10; j++){
-                for (int k = j; k <10; k++){
-                    dp[i][j] += dp[i - 1][k];
+
+        for (int i = 2; i<=n;i++){
+            for (int j = 0; j < 10; j++){
+                for (int k = j; k < 10; k++){
+                    dp[i][j] = (dp[i][j] + dp[i - 1][k]) % MOD;
                 }
-                dp[i][j] %= 10007;
             }
         }
-        int ans = 0;
-        for (int i = 0; i < 10; i++){
-            ans += dp[n][i];
-        }
-        System.out.println(ans % 10007);
+
+        System.out.println(Arrays.stream(dp[n]).sum() % MOD);
+
     }
+
 }
-// 0 1 2 3 4 5 6 7 8 9
-// 00 01 02 03 04 05 06 08.. / 11~19 / 22~ 29
-// 000 001 002
