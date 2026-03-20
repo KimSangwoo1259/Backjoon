ca6272f46f06ccb32be539e30c364e2aee387dfb
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-03-20 14:36:45 +0900

    [level 2] Title: H-Index, Time: 0.97 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,25 +1,15 @@
 import java.util.Arrays;
 
-
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
+        int ans = 0;
         Arrays.sort(citations);
 
-        boolean found = false;
-        for (int i = 0; i < citations.length ; i++) {
-            if (citations[citations.length - 1 - i] < i + 1){
-                if (i == 0)
-                    return 0;
-                found = true;
-                answer = Math.min(citations[citations.length - i], i);
-                break;
-                
-            }
+        int len = citations.length;
+
+        for (int i = 0; i < len; i++){
+            ans = Math.max(ans, Math.min(citations[i], (len - i)));
         }
-        if (!found){
-            answer = citations.length;
-        }
-        return answer;
+        return ans;
     }
 }
