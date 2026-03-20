38f5fb7bcf0cae6f6c7e251b0d14e1c04a455262
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-03-20 14:11:00 +0900

    [level 2] Title: 숫자의 표현, Time: 1.40 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] sum = new int[n+1];
-        sum[1] = 1;
-        for(int i=2;i<=n;i++){
-            sum[i] = sum[i - 1] + i;
-        }
-        int left = 0;
-        int right = 0;
-
-        while(left<=right && right<=n){
-            int temp = sum[right] - sum[left];
-            if (temp < n)
-                right++;
-            else if(temp > n)
-                left++;
-            else{
-                answer++;
-                right++;
+        
+        for (int i = 1; i <=n; i++){
+            int sum =0;
+            for (int j = 0; sum < n; j++){
+                sum += (i + j);
             }
+            if (sum == n)
+                answer++;
+            
         }
+        
         return answer;
     }
 }
