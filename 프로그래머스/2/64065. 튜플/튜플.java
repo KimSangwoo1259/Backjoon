28b64ee515aebd6a1bc151f40e4da7985dacc094
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-03-20 15:01:47 +0900

    [level 2] Title: 튜플, Time: 85.47 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,35 +1,30 @@
 import java.util.*;
 
+
 class Solution {
     public int[] solution(String s) {
-        int[] answer;
-        List<Integer> list = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        Set<Character> charSet = new HashSet<>();
-        charSet.add('{');
-        charSet.add('}');
-        charSet.add(',');
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) - '0' >= 0 && s.charAt(i) - '0' <= 9) {
-                sb.append(s.charAt(i));
-            }
-            else {
-                if (sb.length() > 0) {
-                    int key = Integer.parseInt(sb.toString());
-                    map.put(key, map.getOrDefault(key, 0) + 1);
-                    sb.setLength(0);
-                }
 
-            }
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        s =s.replace("{", "");
+        s = s.replace("}", "");
+        String[] split = s.split(",");
+
+        for (String string : split) {
+            Integer key = Integer.valueOf(string);
+
+            countMap.put(key, countMap.getOrDefault(key, 0) + 1);
         }
-        Set<Integer> integers = map.keySet();
-        answer = new int[integers.size()];
-        for (Integer key : integers) {
-            answer[integers.size() - map.get(key)] = key;
+        List<Integer> keyList = new ArrayList<>(countMap.keySet());
+
+        keyList.sort((k1,k2) -> countMap.get(k2) - countMap.get(k1));
+
+        int size = keyList.size();
+        int[] answer = new int[size];
+
+        for (int i = 0; i < size; i++){
+            answer[i] = keyList.get(i);
         }
-
-
         return answer;
     }
-}// 4 -> 0 3 -> 1 2 ->2 1 -> 3
+}
