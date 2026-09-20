40069108d5000de738b528fe54e25cad71224a96
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-20 16:45:18 +0900

    [level 2] Title: 올바른 괄호, Time: 10.04 ms, Memory: 57.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,30 +1,36 @@
-import java.util.*;
+import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
+        boolean answer = true;
+
         Stack<Character> stack = new Stack<>();
-        int index = 0;  
         
-        while(index < s.length()){
-            char c = s.charAt(index);
-            if(c == '('){
-                stack.push(c);
-            }
-            else {
-                if(stack.isEmpty() || stack.peek() != '('){
-                    return false;
+        // ( 는 그냥 넣기 
+        // ) 라면 먼저 비어있는지 체크, 비어있다면 false, 비어있더라고 peek 가 ( 가 아니라면 false ( 면 pop
+        // 다 하고나서 안 비어있으면 false
+        
+        for (int i = 0; i < s.length(); i++){
+            char now = s.charAt(i);
+            if (now == '(')
+                stack.push(now);
+            else{
+                if (stack.isEmpty()){
+                    answer = false;
+                    break;
                 }
-                else {
+                if (stack.peek() == '('){
                     stack.pop();
                 }
+                else {
+                    answer = false;
+                    break;
+                }
             }
-            index++;
         }
-        if(!stack.isEmpty()){
-            return false;
-        }
-        
+        if (!stack.isEmpty())
+            answer = false;
 
-        return true;
+        return answer;
     }
 }
