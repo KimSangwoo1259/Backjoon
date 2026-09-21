38b5cd32e543df49f8af8efc442cd243bac6e046
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-21 16:02:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.20 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,29 +1,42 @@
+
 import java.util.*;
 
 class Solution {
-   public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
+    public int[] solution(int n, String[] words) {
+        char prevLastChar = words[0].charAt(words[0].length() - 1);
+        int[] answer = new int[2]; // [번호, 차례]
 
-        int[] count = new int[n];
-        Set<String> spokeWords = new HashSet<>();
-        String beforeWord = words[0];
-        count[0]++;
-spokeWords.add(beforeWord);
+        int index = -1;
+        Set<String> usedWords = new HashSet<>();
 
-        for (int i = 1; i < words.length; i++) {
-            if (!spokeWords.contains(words[i]) && beforeWord.charAt(beforeWord.length() - 1) == words[i].charAt(0)) {
-                spokeWords.add(words[i]);
-                count[i%n]++;
-                beforeWord = words[i];
+        usedWords.add(words[0]);
+
+        for (int i = 1; i < words.length; i++){
+            String now = words[i];
+
+            if (usedWords.contains(now) || prevLastChar != now.charAt(0)){
+                index = i;
+                break;
             }
-            else{
-                answer[0] = i % n + 1;
-                count[i%n]++;
-                answer[1] = count[i % n];
-                 break;
-            }
+
+            usedWords.add(now);
+            prevLastChar = now.charAt(now.length() - 1);
         }
+
+        if (index == -1){
+            return answer;
+        }
+
+
+
+        // 번호 -> index % 3 + 1
+        // 몇 번째? index / 3 + 1
+
+        answer[0] = index % n + 1;
+        answer[1] = index / n + 1;
 
         return answer;
     }
 }
+// 이미 사용된 단어 X
+// 바로 전 거랑 앞거랑 이어져야함
