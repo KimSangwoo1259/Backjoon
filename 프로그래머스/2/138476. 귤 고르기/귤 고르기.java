21fe1536d5cc61a872a4f39d4c8ed58f4fc298c6
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-21 14:26:52 +0900

    [level 2] Title: 귤 고르기, Time: 47.74 ms, Memory: 104 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,48 +1,28 @@
 import java.util.*;
-class Solution {
+
+class Solution
+{
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Arrays.sort(tangerine);
-        PriorityQueue<Tang> pq = new PriorityQueue<>();
-        int count = 1;
-        int curNum = tangerine[0];
-        for (int i = 1; i < tangerine.length; i++) {
-            if (curNum != tangerine[i]) {
-                pq.add(new Tang(curNum, count));
-                count = 1;
-                curNum = tangerine[i];
-            }
-            else
-                count++;
-        }
-        pq.add(new Tang(curNum, count));
+        Map<Integer, Integer> tanMap = new HashMap<>();
         
-        int ansCount = 0;
-        while (ansCount < k){
-            ansCount += pq.poll().quantity;
-            answer++;
+        for (int i = 0; i < tangerine.length; i++){
+            int tanSize = tangerine[i];
+            tanMap.put(tanSize, tanMap.getOrDefault(tanSize, 0) + 1);
         }
+        int[] tanValues = tanMap.values().stream().mapToInt(o -> o).toArray();
 
+        Arrays.sort(tanValues);
+        
+        int sum = 0;
+        for (int i = tanValues.length - 1; i >=0; i --){
+            sum += tanValues[i];
+            answer++;
+            if (sum >= k)
+                break;
+        }
+        
         return answer;
     }
-    static class Tang implements Comparable<Tang>{
-        int size;
-        int quantity;
-
-        public Tang(int size, int quantity) {
-            this.size = size;
-            this.quantity = quantity;
-        }
-
-        @Override
-        public int compareTo(Tang o) {
-            if(this.quantity > o.quantity){
-                return -1;
-            }
-            else if(this.quantity < o.quantity){
-                return 1;
-            }
-            return 0;
-        }
-    }
+    
 }
