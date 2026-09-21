390d230a1274fe92a1861cc18d3b036ada9b6615
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-21 15:50:36 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 53.98 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,22 +1,36 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
+        int len = elements.length;
+
         Set<Integer> set = new HashSet<>();
-        final int LEN = elements.length;
-        int[] sum = new int[LEN+1];
-        sum[1] = elements[0];
-        for (int i = 2; i <= LEN; i++){
-            sum[i] = sum[i - 1] + elements[i - 1];
-        }
-        int totalSum = sum[LEN];
-        for (int i = 1; i <= LEN; i++){
-            for (int j = i; j <= LEN; j++){
-                int temp = sum[j] - sum[i - 1];
-                set.add(temp);
-                set.add(totalSum - temp);
-            }
+
+
+        int[] newElements = new int[len + 1];
+        for (int i = 1; i <= len; i++){
+            newElements[i] = elements[i - 1];
         }
 
-        return set.size()-1; // 0 제외
+        int[] sum = new int[len+1];
+
+        for (int i= 1; i <=len;i ++){
+            sum[i] = newElements[i] + sum[i - 1];
+        }
+        int totalSum = sum[len];
+
+        //i : 길이 j : 시작점
+        for (int i = 1; i <= len; i ++){
+            for (int j = 0; j + i <= len; j++){
+                int partSum = sum[i + j] - sum[j];
+                int otherPartSum = totalSum - partSum;
+
+                set.add(partSum);
+                set.add(otherPartSum);
+            }
+        }
+        set.remove(0);
+
+        return set.size();
     }
 }
