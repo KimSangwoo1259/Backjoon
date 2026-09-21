8c62bc80b6336d1b3e90319058be21bf596fb645
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-21 14:47:52 +0900

    [level 2] Title: 구명보트, Time: 8.86 ms, Memory: 58.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,23 +1,33 @@
 import java.util.*;
 
-class Solution {
+
+class Solution
+{
+    // 정렬 -> 가장 작은 사람 + 가장 큰 사람 가능여부? 리밋에 가깝게 하는게 나을거 같긴한데 30 40  60 70 80
+    // 두포인터로 양끝으로 가서 만약 ㄱㄴ? 그러면 태우기 그리고 left ++  right --
     public int solution(int[] people, int limit) {
-        int answer = 0;
+        int twoCount = 0;
+
         Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
 
-        int left = 0, right = people.length - 1;
+        while(left < right){
+            int leftWeight = people[left];
+            int rightWeight = people[right];
+            int totalWeight = leftWeight + rightWeight;
 
-        while (left < right) {
-            int weight = people[left] + people[right];
-            if (weight > limit){
+            if (totalWeight <= limit){
+                twoCount++;
+                left ++;
                 right--;
             }
-            else{
-                answer++;
-                left++;
+            else {
                 right--;
             }
         }
-        return people.length - answer;
+
+        return people.length - twoCount;
     }
+
 }
