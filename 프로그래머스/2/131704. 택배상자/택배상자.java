b577bf4637b9de3dd377093344d558fc9a390967
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-22 15:05:31 +0900

    [level 2] Title: 택배상자, Time: 137.67 ms, Memory: 182 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,40 +1,49 @@
-import java.util.*;
+import java.util.Stack;
+
 
 class Solution {
     public int solution(int[] order) {
         int answer = 0;
-        final int LEN = order.length;
-        int[] boxes = new int[LEN];
-        for (int i = 0; i < LEN; i++){
-            boxes[i] = i+1;
-        }
-        Stack<Integer> st = new Stack<>();
 
-        for (int i = 0; i < LEN; i++){
-            if (boxes[i] == order[answer]){
-                answer++;
+        int len = order.length;
+
+        Stack<Integer> stack1 = new Stack<>();
+
+        Stack<Integer> stack2 = new Stack<>();
+
+        for (int i = len; i >= 1; i --){
+            stack1.push(i);
+        }
+
+        for (int i = 0; i < len; i++){
+            int now = order[i];
+            if (stack1.isEmpty()){
+                if (stack2.peek() == now){
+                    answer++;
+                    stack2.pop();
+                }
+                else
+                    break;
             }
             else {
-                while (!st.isEmpty()){
-                    if (st.peek() == order[answer]){
-                        st.pop();
-                        answer++;
-                    }
-                    else
-                        break;
-                }
-                st.push(boxes[i]);
+               while(!stack1.isEmpty()){
+                   if (stack1.peek() == now){
+                       stack1.pop();
+                       answer++;
+                       break;
+                   }
+                   if (!stack2.isEmpty() && stack2.peek() == now){
+                       stack2.pop();
+                       answer++;
+                       break;
+                   }
+                   stack2.push(stack1.pop());
+               }
+                
+                
             }
+          
 
-
-        }
-        while (!st.isEmpty()){
-            if (st.peek() == order[answer]){
-                st.pop();
-                answer++;
-            }
-            else
-                break;
         }
 
         return answer;
