29af15837f51487427de0b324e97eb906a82a58a
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-22 13:05:00 +0900

    [level 2] Title: 기능개발, Time: 4.56 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,31 +1,42 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] days = new int[progresses.length];
-        List<Integer> ans = new ArrayList<>();
-        final int complete = 100;
-        for (int i = 0; i < progresses.length; i++) {
-            int remain = complete - progresses[i];
-            days[i] += remain / speeds[i];
-            if (remain % speeds[i] != 0)
-                days[i]++;
-        }
-        Stack<Integer> st = new Stack<>();
-        st.push(days[0]);
-        int max = days[0];
-        for (int i = 1; i < days.length; i++){
-            if (days[i] > max){
-                max = days[i];
-                ans.add(st.size());
-                st = new Stack<>();
-                st.add(days[i]);
-            }
-            else
-                st.push(days[i]);
-        }
-        ans.add(st.size());
-        int[] answer = ans.stream().mapToInt(Integer::intValue).toArray();
-        return answer;
+        int[] answer = {};
+        int len = progresses.length;
+        
+        // 걸리는 날짜들을 배열로 만들기
+        // 연속으로 감소하는수열이 어디서 끊기는지 보는거랑 똑같은거 같은디??
+        int[] days = new int[len];
+        
+        for (int i = 0; i < len; i++){
+            int remainProcess = 100 - progresses[i];
 
+            int day = remainProcess / speeds[i];
+            
+            if (remainProcess % speeds[i] != 0)
+                day++;
+
+            days[i] = day;
+        }
+        List<Integer> ans = new ArrayList<>();
+        int count = 1;
+        int curValue = days[0];
+        
+        int i = 1;
+        while(i < len){
+            if (days[i] > curValue){
+                ans.add(count);
+                curValue = days[i];
+                count = 1;
+            }
+            else {
+                count++;
+            }
+            i++;
+        }
+        ans.add(count);
+        
+        return ans.stream().mapToInt(a->a).toArray();
     }
 }
