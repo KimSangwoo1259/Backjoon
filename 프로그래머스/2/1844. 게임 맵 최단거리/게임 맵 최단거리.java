c105ae85b2d62a55f0a4698c529ff192db9ed3b7
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-22 13:25:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.14 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,37 +2,50 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 1000000000;
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-        boolean[][] visited = new boolean[maps.length][maps[0].length];
-        Queue<Me> q = new LinkedList<>();
-        q.add(new Me(0, 0, 0));
-        while (!q.isEmpty()) {
+        final int FAIL = 100000;
+        int answer = FAIL;
+
+
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        Queue<Me> q = new ArrayDeque<>();
+
+        q.add(new Me(0, 0, 1));
+        visited[0][0] = true;
+        int[] dr = {-1, 0, 1, 0};
+        int[] dc = {0, 1, 0, -1};
+
+
+
+        while(!q.isEmpty()){
             Me now = q.poll();
-            if (now.x == maps.length - 1 && now.y == maps[0].length - 1) {
-                answer = Math.min(answer, now.count);
-                continue;
+            if (now.x == n -1 && now.y == m - 1){
+                answer = Math.min(now.count, answer);
             }
-            for (int i = 0; i < 4; i++) {
-                int nx = now.x + dr[i];
-                int ny = now.y + dc[i];
-                if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length) {
-                    if(!visited[nx][ny] && maps[nx][ny] == 1) {
+
+            else {
+                for (int i = 0; i < 4; i++) {
+                    int nx = now.x + dr[i];
+                    int ny = now.y + dc[i];
+
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] != 0 && !visited[nx][ny]) {
                         visited[nx][ny] = true;
                         q.add(new Me(nx, ny, now.count + 1));
                     }
                 }
             }
+
         }
-        if (answer == 1000000000)
-            return -1;
-        
-        return answer+1;
+
+        return (answer != FAIL ? answer : -1);
     }
-    static class Me {
-        int x,y,count;
-        Me(int x, int y, int count) {
+    public class Me {
+        int x;
+        int y;
+        int count;
+
+        public Me(int x, int y, int count) {
             this.x = x;
             this.y = y;
             this.count = count;
