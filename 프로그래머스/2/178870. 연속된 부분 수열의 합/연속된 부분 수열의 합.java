bfa5df5c9113798df7417824ad5b14be88b7777a
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-24 18:25:33 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 14.85 ms, Memory: 168 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -1,43 +1,44 @@
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        final int LEN = sequence.length;
-            int[] sum = new int[LEN];
-            sum[0] = sequence[0];
-            for (int i = 1; i < LEN; i++){
-                sum[i] = sum[i - 1] + sequence[i];
+        int[] answer = new int[2];
+
+        int[] sum = new int[sequence.length];
+        int len = 10000000;
+        sum[0] = sequence[0];
+
+        for (int i = 1; i < sequence.length; i++){
+            sum[i] = sum[i - 1] + sequence[i];
+        }
+
+        int left = 0; int right = 0;
+
+        while(left <= right && right < sequence.length){
+            int temp;
+            if (left == 0){
+                temp = sum[right];
             }
+            else {
+                temp = sum[right] - sum[left - 1];
+            }
+            if (temp > k){
+                left++;
 
-            int lAns = 100000000;
-            int rAns = 200000000;
+            }else if (temp < k){
+                right++;
+            }
+            else {
+                int tempLen = right - left;
 
-            int left = 0;
-            int right = 0;
-
-            while (left <= right && left < LEN && right < LEN){
-
-                int temp;
-                if (left == 0)
-                    temp = sum[right];
-                else
-                    temp = sum[right] - sum[left - 1];
-                if (temp <= k){
-                    if (temp == k){
-                        if (right - left < rAns - lAns){
-                            rAns = right;
-                            lAns = left;
-                        }
-                    }
-                    right++;
+                if (tempLen < len){
+                    answer[0] = left;
+                    answer[1] = right;
+                    len = tempLen;
                 }
-                else
-                    left++;
-
-
-
+                right++;
+                left++;
             }
-            int[] answer = new int[2];
-            answer[0] = lAns;
-            answer[1] = rAns;
-            return answer;
+        }
+
+        return answer;
     }
 }
