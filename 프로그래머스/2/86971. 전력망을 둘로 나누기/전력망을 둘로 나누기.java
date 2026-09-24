b7562feed44c84e09d9896c18a2643a2f2309630
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-24 19:52:30 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 7.90 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,57 +1,63 @@
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        int answer = 100;
+        int answer = n;
 
-        int[][] graph = new int[n + 1][n + 1];
-        
-        for (int i = 0; i < wires.length; i++){
-            int a = wires[i][0];
-            int b = wires[i][1];
-            
-            graph[a][b] = 1;
-            graph[b][a] = 1;
+        boolean[][] graph = new boolean[n + 1][n + 1];
+
+        // 초기값 설정
+        for (int i = 0 ; i < n-1; i++){
+            int left = wires[i][0];
+            int right = wires[i][1];
+
+            graph[left][right] = true;
+            graph[right][left] = true;
+
         }
-        
-        for (int i = 0; i < n-1; i++){
-            boolean[] visited = new boolean[n + 1];
-            int a = wires[i][0];
-            int b = wires[i][1];
-            
-            graph[a][b] = 0;
-            graph[b][a] = 0;
 
-            int[] count = new int[2];
-            int cnt = 0;
-            for (int j = 1; j <= n; j++){
-                int temp = 0;
-                if (!visited[j]){
-                    visited[j] = true;
-                    Queue<Integer> q = new LinkedList<>();
-                    q.add(j);
-                    while(!q.isEmpty()){
-                        Integer now = q.poll();
-                        temp++;
-                        for (int k = 1; k <= n; k++){
-                            if (!visited[k] && graph[k][now] == 1){
-                                q.add(k);
-                                visited[k] = true;
-                            }
-                        }
-                    }
-                    if (temp != 0){
-                        count[cnt] = temp;
-                        cnt++;
+        // for 문 돌면서 하나씩 끊어 보면서 값 비교
+        for (int i = 0; i < n-1; i++){
+            int left = wires[i][0];
+            int right = wires[i][1];
+
+
+            graph[left][right] = false;
+            graph[right][left] = false;
+
+            boolean[] visited = new boolean[n + 1];
+            int count = 0;
+
+            Queue<Integer> queue = new ArrayDeque<>();
+
+            queue.add(1);
+            visited[1] = true;
+
+            while(!queue.isEmpty()){
+                Integer now = queue.poll();
+                count++;
+                for (int j = 1; j <=n; j++){
+                    if (!visited[j] && graph[now][j]){
+                        queue.add(j);
+                        visited[j] = true;
                     }
                 }
             }
-            graph[a][b] = 1;
-            graph[b][a] = 1;
-            answer = Math.min(answer, Math.abs(count[0] - count[1]));
-            
+
+            int dif = Math.abs(count - (n - count));
+
+            if (dif < answer){
+                answer = dif;
+            }
+
+
+            graph[left][right] = true;
+            graph[right][left] = true;
+
         }
-        
+
+
         return answer;
     }
 }
