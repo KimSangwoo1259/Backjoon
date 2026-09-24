3b54d0f25b22723824b581e36687f0c2602ad0b4
KimSangwoo1259 <72953900+KimSangwoo1259@users.noreply.github.com>
2026-09-24 19:29:29 +0900

    [level 2] Title: 시소 짝꿍, Time: 15.84 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.java
@@ -1,38 +1,50 @@
+import java.util.*;
+
 class Solution {
     public long solution(int[] weights) {
         long answer = 0;
-            long[] count = new long[1001];
-            for (int i = 0; i < weights.length; i++){
-                count[weights[i]]++;
+
+        Map<Integer,Integer> weightMap =new HashMap<>();
+
+        for (int i = 0; i < weights.length; i++){
+            weightMap.put(weights[i], weightMap.getOrDefault(weights[i], 0) + 1);
+        }
+        List<Integer> keyList = new ArrayList<>(weightMap.keySet());
+        Collections.sort(keyList);
+
+        for (int key: keyList){
+            long count = weightMap.get(key);
+
+            // case 1: 자기들 끼리(1)
+            if (count >= 2){
+                answer += (count * (count - 1)) / 2;
             }
 
-            for (int i = 100; i <= 1000; i++){
-                if (count[i] >= 1){
-                    long temp;
-                    answer += (count[i] * (count[i] - 1)) / 2;
-                    if (i % 3 == 0){
-                        temp = i / 3 * 4;
-                        if (temp<= 1000){
-                            if (count[(int) temp] >= 1) {
-                                answer += count[i] * count[(int) temp];
-                            }
-                        }
-                    }
-                    if (i % 2 == 0){
-                        temp = i / 2 * 3;
-                        if (temp<= 1000){
-                            if (count[(int) temp] >= 1) {
-                                answer += count[i] * count[(int) temp];
-                            }
-                        }
-                    }
-                    if (i * 2 <= 1000){
-                        temp = i * 2;
-                        if (count[(int) temp] >= 1)
-                            answer += count[i] * count[(int)temp];
-                    }
-                }
+            // case 2: 1.5배
+            if (key % 2 == 0 && key /2 * 3 <= 1000){
+                long oppCount = weightMap.getOrDefault(key / 2 * 3,0);
+
+
+                answer += (count * oppCount);
             }
-            return answer;
+
+
+            // case 3: 2 배
+            if (key * 2 <= 1000){
+                long oppCount = weightMap.getOrDefault(key * 2,0);
+
+                answer += (count * oppCount);
+            }
+
+
+            // case 4: 1.333333... 배
+            if (key % 3 == 0 && key / 3 * 4 <=1000){
+                long oppCount = weightMap.getOrDefault(key / 3 * 4,0);
+
+                answer += (count * oppCount);
+            }
+        }
+
+        return answer;
     }
 }
